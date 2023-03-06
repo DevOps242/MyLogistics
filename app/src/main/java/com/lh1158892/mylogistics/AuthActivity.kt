9386344b998954ea65ddb12f49bc0dea4b5a0653
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2023-03-06 14:44:06 -0500

    Added Firebase Authentication and start Activity.

---
+++ app/src/main/java/com/lh1158892/mylogistics/AuthActivity.kt
@@ -0,0 +1,60 @@
+package com.lh1158892.mylogistics
+
+import android.app.Application
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Choose authentication providers
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+        )
+
+        // Create and launch sign-in intent
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+//            .setLogo(R.drawable.time_tracker_logo)
+            .build()
+        signInLauncher.launch(signInIntent)
+    }
+
+    // See: https://developer.android.com/training/basics/intents/result
+    private val signInLauncher = registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        this.onSignInResult(res)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            // Successfully signed in
+            val user = FirebaseAuth.getInstance().currentUser
+            val intent = Intent(this, MainActivity::class.java)
+
+            intent.putExtra("user", user)
+            startActivity(intent)
+
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise check
+            // response.getError().getErrorCode() and handle the error.
+            // ...
+            Toast.makeText(this, "Authentication Failed", Toast.LENGTH_LONG).show()
+            startActivity(Intent(this, AuthActivity::class.java))
+        }
+    }
+
+}
