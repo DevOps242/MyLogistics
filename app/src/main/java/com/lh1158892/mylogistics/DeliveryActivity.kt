c82cecd7deac710d504936587b3ee4f5f46e7c18
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2023-03-06 15:42:08 -0500

    Building Navigation.

---
+++ app/src/main/java/com/lh1158892/mylogistics/DeliveryActivity.kt
@@ -0,0 +1,11 @@
+package com.lh1158892.mylogistics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DeliveryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_delivery)
+    }
+}
