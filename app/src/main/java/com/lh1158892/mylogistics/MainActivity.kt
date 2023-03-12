3a9f6305c32d20cff23b76eab785d8fcd37aa307
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2023-03-12 19:35:58 -0400

    Update: UI; Added scroll view to main activity; Corrected recycler view paddings; change icon color.

--- app/src/main/java/com/lh1158892/mylogistics/MainActivity.kt
+++ app/src/main/java/com/lh1158892/mylogistics/MainActivity.kt
@@ -3,11 +3,9 @@ package com.lh1158892.mylogistics
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.activity.viewModels
+import com.lh1158892.mylogistics.Adapters.ParcelAdapter
+import com.lh1158892.mylogistics.ViewModels.ParcelViewModel
 import com.lh1158892.mylogistics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -19,6 +17,23 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         var currentActivity = 0;
 
+
+        val viewModel : ParcelViewModel by viewModels()
+        viewModel.getParcels().observe(this, {
+            binding.parcelRecyclerViewMain.adapter = ParcelAdapter(this,it)
+        })
+
+        // Parcel View All Button
+        binding.viewAllParcelButton.setOnClickListener {
+            val intent = Intent(this, ParcelActivity::class.java)
+            startActivity(intent)
+        }
+
+        // Deliveries View All Button
+        binding.viewAllDeliveriesButton.setOnClickListener {
+            val intent = Intent(this, DeliveryActivity::class.java)
+            startActivity(intent)
+        }
 
         // Handle Navigation Actions for the Activities
         var navigationHandler = binding.bottomNavigationView
@@ -32,22 +47,18 @@ class MainActivity : AppCompatActivity() {
                     when (index) {
                         0 -> {
                             val intent = Intent(this, MainActivity::class.java)
-//                            intent.putExtra("user",user)
                             startActivity(intent)
                         }
                         1 -> {
                             val intent = Intent(this, ParcelActivity::class.java)
-//                            intent.putExtra("user",user)
                             startActivity(intent)
                         }
                         2 -> {
                             val intent = Intent(this, DeliveryActivity::class.java)
-//                            intent.putExtra("user",user)
                             startActivity(intent)
                         }
                         3 -> {
                             val intent = Intent(this, AccountSettingActivity::class.java)
-//                            intent.putExtra("user",user)
                             startActivity(intent)
                         }
                     }
@@ -55,17 +66,7 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        // Parcel View All Button
-        binding.viewAllParcelButton.setOnClickListener {
-            val intent = Intent(this, ParcelActivity::class.java)
-            startActivity(intent)
-        }
 
-        // Deliveries View All Button
-        binding.viewAllDeliveriesButton.setOnClickListener {
-            val intent = Intent(this, DeliveryActivity::class.java)
-            startActivity(intent)
-        }
 
     }
 }
