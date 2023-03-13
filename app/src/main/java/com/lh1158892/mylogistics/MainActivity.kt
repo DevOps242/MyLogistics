ce5e4bbb9125513d7be4262af98b2124b3799569
Akeem Palmer <42576837+DevOps242@users.noreply.github.com>
2023-03-13 16:27:18 -0400

    Update: the functionality to search a parcel with tracking number.

--- app/src/main/java/com/lh1158892/mylogistics/MainActivity.kt
+++ app/src/main/java/com/lh1158892/mylogistics/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.lh1158892.mylogistics
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import androidx.activity.viewModels
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import com.lh1158892.mylogistics.Adapters.ParcelAdapter
 import com.lh1158892.mylogistics.Models.Parcel
 import com.lh1158892.mylogistics.ViewModels.ParcelViewModel
@@ -22,12 +27,7 @@ class MainActivity : AppCompatActivity(), ParcelAdapter.ParcelItemListener {
         //create a list of Projects to populate the Project Spinner
         var parcels : MutableList<Parcel> = ArrayList()
 
-        // This currently gets all.
-//        val viewModel : ParcelViewModel by viewModels()
-//        viewModel.getParcels().observe(this, {
-//            binding.parcelRecyclerViewMain.adapter = ParcelAdapter(this,it)
-//        })
-
+        var user = FirebaseAuth.getInstance().currentUser
 
         val viewModel : ParcelViewModel by viewModels()
         viewModel.getParcels().observe(this) {
@@ -39,7 +39,43 @@ class MainActivity : AppCompatActivity(), ParcelAdapter.ParcelItemListener {
                     }
                 }
             }
+        }
 
+        // Tracking Parcel Search
+        binding.trackParcelButton.setOnClickListener {
+            // get the tracking id text from user.
+            var trackingIdField = binding.trackParcelEditText.text
+            var trackingId = trackingIdField.toString()
+
+            if (trackingId.isNullOrBlank()){
+                Toast.makeText(this, "Tracking Number Input must not be blank", Toast.LENGTH_LONG).show()
+            } else {
+
+                Log.i("DB_Response", "your tracking id: $trackingId")
+                // Perform a search
+                val db = FirebaseFirestore.getInstance().collection("parcels")
+                    .whereEqualTo("recipientId", user.uid)
+                    .whereEqualTo("trackingId", trackingId)
+                    .get()
+                    .addOnSuccessListener {
+                        if (it.isEmpty) {
+                            // Doesn't Exists
+                            Toast.makeText(this, "There is no parcel with this tracking number", Toast.LENGTH_LONG).show()
+
+                            // Removes the keyboard.
+                            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                            inputMethodManager.hideSoftInputFromWindow(binding.mainActivity.windowToken, 0)
+                        } else {
+                            // Parcel Exists
+                            val documentSnapshot = it.documents[0]
+                            var parcelId = documentSnapshot.id
+
+                            val intent = Intent(this, ParcelDetailActivity::class.java)
+                            intent.putExtra("documentId", parcelId)
+                            startActivity(intent)
+                        }
+                    }
+            }
         }
 
         // Parcel View All Button
@@ -84,9 +120,6 @@ class MainActivity : AppCompatActivity(), ParcelAdapter.ParcelItemListener {
                 }
             }
         }
-
-
-
     }
 
     override fun parcelSelected(parcel: Parcel) {
